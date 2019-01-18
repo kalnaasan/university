5629af2e363e59a33a92db816a365b8ebf537b8f
Kaddour Alnaasan <qaduralnaasan@gmail.com>
2019-01-18 20:10:15 +0100

    The ninth exercise of PRG

---
+++ Programmieren 1/PRG1/Übungen/Übung_09/Alnaasan_Kaddour_0016285.py
@@ -0,0 +1,47 @@
+""" This script is the solution of Exercise Sheet No.9"""
+
+__author__ = "0016285: Kaddour Alnaasan"
+__credits__ = """If you would like to thank somebody
+              i.e. an other student for her code or leave it out"""
+__email__ = "qaduralnaasan@gmail.com"
+
+
+class Person():
+    """Docstring: aAbstrckt Class, that properties of Person"""
+
+    def __init(self):
+        pass
+
+
+class Student(Person):
+    """Docstring: """
+    def __init__(self):
+        pass
+
+
+class Car():
+    """Docstring: """
+
+    def __init__(self):
+        pass
+
+
+class Owner():
+    """Docstring: """
+
+    def __init__(self):
+        pass
+
+
+class SoundBoard():
+    """Docstring: """
+
+    def __init__(self):
+        pass
+
+
+class Guitar():
+    """Docstring: """
+
+    def __init__(self):
+        self.sound_board = SoundBoard()
